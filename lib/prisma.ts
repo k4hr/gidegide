94bd285e20104b2b98b13169bc9b5877524e0969
k4hr <alecsandr-7a@yandex.ru>
2026-05-02 04:09:34 +0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,15 +4,15 @@ const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+function createPrismaClient() {
+  return new PrismaClient({
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
   });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
 }
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+globalForPrisma.prisma = prisma;
